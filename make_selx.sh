0436e48a03d50cb751451bbe613585b4ed2e4944
Brian Keating <brikeats@gmail.com>
2021-03-17 11:45:10 -0700

    debug more

--- make_selx.sh
+++ make_selx.sh
@@ -12,6 +12,12 @@ PYTHON_EXE=$PYTHON_BIN/python
 INCLUDES=(/opt/python/$PYTHON_TARGET/include/*) # all subdirs
 PYTHON_INCLUDE=${INCLUDES[0]} # get the appropriate include directory
 
+# DEBUG
+pwd
+ls 
+ls ..
+
+
 cd ../source
 mkdir build
 cd build
